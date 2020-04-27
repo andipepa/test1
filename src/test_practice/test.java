6ad60b0639fbb9398375dbaaed37fe8735473dc5
Andi Pepa <andipepa87.ap@gmail.com>
2020-04-27 13:14:51 -0400

    Here message again

--- src/test_practice/test.java
+++ src/test_practice/test.java
@@ -2,6 +2,6 @@ package test_practice;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello Andi Pepa here");
+        System.out.println("Hello Andi Pepa");
     }
 }
