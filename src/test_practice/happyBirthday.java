daad5a5e7d96a8e281a3a99ce7f59ea4a36d94cd
andipepa <64115365+andipepa@users.noreply.github.com>
2020-04-28 15:49:28 -0400

    Update happyBirthday.java

--- src/test_practice/happyBirthday.java
+++ src/test_practice/happyBirthday.java
@@ -3,6 +3,8 @@ package test_practice;
 public class happyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy bithday man");
+        
+        //you need to change smthng here man,my birthday is next week :)
     }
 
 }
