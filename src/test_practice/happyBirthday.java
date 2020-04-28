f10fc19ea30a70dd33f69874f3c3ddfdfed5102c
Andi Pepa <andipepa87.ap@gmail.com>
2020-04-28 15:44:22 -0400

    Happy birthday

---
+++ src/test_practice/happyBirthday.java
@@ -0,0 +1,8 @@
+package test_practice;
+
+public class happyBirthday {
+    public static void main(String[] args) {
+        System.out.println("Happy bithday man");
+    }
+
+}
--- src/test_practice/test1.java
+++ src/test_practice/test1.java
@@ -3,5 +3,7 @@ package test_practice;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Java is really fun");
+
+        //i want to modivie
     }
 }
